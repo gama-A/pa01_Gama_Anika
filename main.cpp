427b09fe899d7f2dea233cf125eb0f2f9d9cf877
Gama Aristondo <gamaliel@cs.ucsb.edu>
2020-05-07 17:29:56 -0700

    first attempt to game

--- main.cpp
+++ main.cpp
@@ -47,9 +47,46 @@ int main(int argv, char** argc){
 
   int gameStatus = 2;
   while (gameStatus > 0){
-      // STUB
+      Card *p1, *p2;
+      int turnCounter = 0;
+      if(turnCounter % 2 == 0){
+          p1 = player1.getCardDeck();
+          for(p1; p1->next != NULL; p1 = p1->next){
+              int result;
+              result = player2.findCard(*p1);
+              if(result != 0){
+                  player2.remove(result);
+                  cout << player1.getName() << " picked matching card " << *p1 << endl;
+                  player1.remove(player1.findCard(*p1));
+                  // STUB
+              }
+          }
+          if(result == 0){
+              gameStatus = gameStatus - 1;
+          }
+          turnCounter = turnCounter + 1;
+      }
+      else {
+          p2 = player2.getCardDeck();
+          for(p2; p2->next != NULL; p2 = p2->next){
+              int result;
+              result = player1.findCard(*p2);
+              if(result != 0){
+                  player1.remove(result);
+                  cout << player2.getName() << " picked matching card " << *p2 << endl;
+                  player2.remove(player2.findCard(*p2));
+                  // Stub
+              }
+          }
+          if(result == 0){
+              gameStatus = gameStatus - 1;
+          }
+          turnCounter = turnCounter + 1;
+      }
   }
-
+  
+  cout << endl;
+  cout << player1.getName() <<"'s cards:\n" << player1 << player2.getName() <<"'s cards:\n" << player2;
 
   return 0;
 }
